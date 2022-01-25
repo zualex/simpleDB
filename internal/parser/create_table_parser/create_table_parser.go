256f58860930b2b693046620e92c3a97341040c3
zualex <Azubarev32@gmail.com>
2022-01-25 10:17:50 +0300

    Validate create table handler

--- internal/parser/create_table_parser/create_table_parser.go
+++ internal/parser/create_table_parser/create_table_parser.go
@@ -2,8 +2,26 @@ package create_table_parser
 
 import (
 	"fmt"
+	"regexp"
 )
 
 func Handle(sqlLowerCase string) {
-	fmt.Println("Hande create table", sqlLowerCase)
+	// tokens := strings.Fields(sqlLowerCase)
+	// fmt.Println(len(tokens))
+	// fmt.Println(tokens[0])
+	// fmt.Println(tokens[1])
+	// fmt.Println(tokens[2])
+	// fmt.Println(tokens[3])
+	// fmt.Println(tokens[4])
+	// fmt.Println(tokens[5])
+
+	isValid(sqlLowerCase)
+}
+
+func isValid(sql string) bool {
+	// TODO получить группы https://pkg.go.dev/regexp#Regexp.SubexpNames
+	match, ok := regexp.MatchString(`^create table(.*?)\((.*?)\);$`, sql)
+	fmt.Println(match, ok)
+
+	return false
 }

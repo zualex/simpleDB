54a8e337c8afc7418a2af68d32498cc107c350a4
zualex <Azubarev32@gmail.com>
2022-02-02 14:16:51 +0300

    Create test for creat_table.go

--- internal/parser/create_table/create_table_test.go
+++ internal/parser/create_table/create_table_test.go
@@ -1,1 +1,36 @@
 package create_table
+
+import "testing"
+
+type createTableToken struct {
+	isValid bool
+	table   string
+	fields  []string
+}
+
+func TestGetTokenSuccess(t *testing.T) {
+	m := map[string]createTableToken{
+		"create table test (id, value);": {true, "test", []string{"id", "value"}},
+	}
+
+	// TODO добавить сценариев
+
+	for sql, expected := range m {
+		isValid, table, fields := getToken(sql)
+		if isValid != expected.isValid {
+			t.Errorf("getToken(\"%s\"); got isValid: %t, expected %t", sql, isValid, expected.isValid)
+		}
+		if table != expected.table {
+			t.Errorf("getToken(\"%s\"); got table: %s, expected %s", sql, table, expected.table)
+		}
+		if len(fields) != len(expected.fields) {
+			t.Errorf("getToken(\"%s\"); got len(fields): %d, expected %d", sql, len(fields), len(expected.fields))
+		}
+
+		for i := range expected.fields {
+			if fields[i] != expected.fields[i] {
+				t.Errorf("getToken(\"%s\"); got fields[%d]: %s, expected %s", sql, i, fields[i], expected.fields[i])
+			}
+		}
+	}
+}
